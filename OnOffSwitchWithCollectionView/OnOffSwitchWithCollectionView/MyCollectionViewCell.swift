2bdbc86b2bcc94f84399d2dd0c85e7065b8610e5
AhmedAshrafFayeq <47187014+AhmedAshrafFayeq@users.noreply.github.com>
2019-06-18 11:52:04 +0200

    Add files via upload

---
+++ OnOffSwitchWithCollectionView/OnOffSwitchWithCollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  OnOffSwitchWithCollectionView
+//
+//  Created by JETS Mobil Lab -  on 5/26/19.
+//  Copyright © 2019 iti. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
